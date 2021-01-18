ef9256e97c5717294a9e75e6d4eb4b4308a40ab2
Karolis Jachimavičius <76951309+KarolisVJ@users.noreply.github.com>
2021-01-18 16:57:22 +0200

    loops

--- js/main.js
+++ js/main.js
@@ -27,9 +27,11 @@ console.log(`${w[0]} plaukia nesroviui`);
 // if () {} else {}
 // if () {} else if () {}
 
-const arLygu = a === b ? true : false;
+// const arLygu = a === b ? true : false;
 
 // SWITCH
+
+let day = 1;
 
 switch (day) {
     case 1:
@@ -42,4 +44,43 @@ switch (day) {
     default:
         console.log('Tokios dienos nėra');
         break;
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log('kartotinas kodas');
+}
+
+// const precision = 100000;
+// const value = Math.round(i*precision)/precision
+
+const words = ['abc', 'Borisas', 'citrina', 'duona', 'ezys']
+
+for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    console.log ('Pirma zodzio raide:', word[0]);
+    console.log('Zodzio ilgis', word.length);
+}
+
+let sum = 0;
+for (let i = 0; i <= 5; i++) {
+    sum +=i;
+    console.log(i, sum);
+}
+
+//Perrasyti teksta is galo i prieki
+
+const text = 'abcdef';
+let reverseText = '';
+const size = text.length;
+
+// for (let i = 0; i < text.length; i++) {
+//     const letter = text[i];
+//     reverseText = letter + reverseText;
+//     console.log(i, letter, reverseText);
+// }
+
+for (let i = 1; i <= size; i++) {
+    const letter = text[size - i];
+    reverseText += letter;
+    console.log(i, letter, reverseText);
 }
